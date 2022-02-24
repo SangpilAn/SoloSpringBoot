a04bafa555cb4020e9be2830211c966ee64e514a
asp4303 <asp4303@gmail.com>
2022-02-24 16:39:41 +0900

    JPA 기본 설정

---
+++ testProject01/src/main/java/com/example/testproject01/jpaTest/repository/MemberRepository.java
@@ -0,0 +1,18 @@
+package com.example.testproject01.jpaTest.repository;
+
+import com.example.testproject01.jpaTest.vo.MemberVo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface MemberRepository extends JpaRepository<MemberVo, Long> {
+
+    public List<MemberVo> findById(String id);
+
+    public List<MemberVo> findByName(String name);
+
+    public List<MemberVo> findByNameLike(String keyword);
+
+}
