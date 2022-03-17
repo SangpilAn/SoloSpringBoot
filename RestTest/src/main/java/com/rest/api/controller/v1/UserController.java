209aee77e3c9c7ba966de7c2dc613809a6172c01
asp4303 <asp4303@gmail.com>
2022-03-17 15:35:39 +0900

    ControllerAdvice를 이용한 Exception처리

--- RestTest/src/main/java/com/rest/api/controller/v1/UserController.java
+++ RestTest/src/main/java/com/rest/api/controller/v1/UserController.java
@@ -1,6 +1,8 @@
 package com.rest.api.controller.v1;
 
+import com.rest.api.advice.exception.CUserNotFoundException;
 import com.rest.api.entity.User;
+import com.rest.api.model.response.CommonResult;
 import com.rest.api.model.response.ListResult;
 import com.rest.api.model.response.SingleResult;
 import com.rest.api.repository.UserJpaRepo;
@@ -22,24 +24,48 @@ public class UserController {
     private final UserJpaRepo userJpaRepo;
     private final ResponseService responseService;
 
-    @ApiOperation(value = "회원 조회", notes = "모든 회원을 조회한다.")
+    @ApiOperation(value = "회원 리스트 조회", notes = "모든 회원을 조회한다.")
     @GetMapping(value = "/user")
     public ListResult<User> findAllUser(){
         return responseService.getListResult(userJpaRepo.findAll());
     }
 
+    @ApiOperation(value = "회원 단건 조회", notes = "userId 로 회원을 조회한다.")
+    @GetMapping(value = "/user/{msrl}")
     public SingleResult<User> findUserById(@ApiParam(value = "회원ID", required = true) @PathVariable long msrl){
-        return responseService.getSingleResult(userJpaRepo.findById(msrl).orElse(null));
+        return responseService.getSingleResult(userJpaRepo.findById(msrl).orElseThrow(CUserNotFoundException::new));
     }
 
     @ApiOperation(value = "회원 입력", notes = "회원을 입력한다.")
     @PostMapping(value = "/user")
-    public User save(@ApiParam(value = "회원아이디", required = true) @RequestParam String uid,
+    public SingleResult<User> save(@ApiParam(value = "회원아이디", required = true) @RequestParam String uid,
                      @ApiParam(value = "회원이름", required = true) @RequestParam String name){
         User user = User.builder()
                 .uid(uid)
                 .name(name)
+                .name(name)
                 .build();
-        return userJpaRepo.save(user);
+        return responseService.getSingleResult(userJpaRepo.save(user));
+    }
+
+    @ApiOperation(value = "회원 수정", notes = "회원정보를 수정한다.")
+    @PutMapping(value = "/user")
+    public SingleResult<User> modify(
+            @ApiParam(value = "회원번호", required = true) @RequestParam long msrl,
+            @ApiParam(value = "회원아이디", required = true) @RequestParam String uid,
+            @ApiParam(value = "회원이름", required = true) @RequestParam String name) {
+        User user = User.builder()
+                .msrl(msrl)
+                .uid(uid)
+                .name(name)
+                .build();
+        return responseService.getSingleResult(userJpaRepo.save(user));
+    }
+
+    @ApiOperation(value = "회원 삭제", notes = "userId로 회원정보를 삭제한다.")
+    @DeleteMapping(value = "/user/{msrl}")
+    public CommonResult delete(@ApiParam(value = "회원 삭제", required = true) @PathVariable long msrl){
+        userJpaRepo.deleteById(msrl);
+        return responseService.getSuccessResult();
     }
 }
